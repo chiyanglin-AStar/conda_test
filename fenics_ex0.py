b732c2a9c0f3150e49a12a40e1d096c879666010
chiyanglin-AStar <chiyanglin@gmail.com>
2024-12-10 03:21:02 +0000

    update

--- fenics_ex0.py
+++ fenics_ex0.py
@@ -1,8 +1,10 @@
-from fenics import *
+#from fenics import *
 import matplotlib.pyplot as plt
 import pandas as pd
+import fenics as fe
 
 
 print("panda verion:",pd.__version__)
 #print("fenics version:",fenics.__version__)
+print("fenics function list :",dir(fe))
 
